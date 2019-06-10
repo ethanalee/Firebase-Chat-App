a49c076697f6079a602a5baed9e03a68b19ac866
Ethan Lee <ethanl@tdlab.io>
2019-06-10 16:52:01 -0400

    added vuex and router

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import './plugins/vuetify'
 import App from './App.vue'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
