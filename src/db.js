a0f612c4169613e53a7690bbff18cc271c057adf
Frank Flitton <frank@tdlab.io>
2019-06-11 10:01:57 -0400

    Vuefire

---
+++ src/db.js
@@ -0,0 +1,53 @@
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
+
+// Get a Firestore instance
+const db = firebase
+  .initializeApp({
+    apiKey: process.env.VUE_APP_FIREBASE_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_KEY_MESSAGING
+  })
+  .firestore()
+
+const GeoPoint = firebase.firestore.GeoPoint
+const TimeStamp = firebase.firestore.TimeStamp
+const CurrentTime = firebase.firestore.FieldValue.serverTimestamp()
+const FieldValue = firebase.firestore.FieldValue
+
+const auth = {
+  emailLogIn: (email, password) => {
+    return firebase
+      .auth()
+      .signInWithEmailAndPassword(
+        email,
+        password
+      )
+  },
+  currentUser: () => {
+    return firebase.auth().currentUser
+  },
+  logOut: () => {
+    return firebase.auth().signOut()
+  }
+}
+
+const storage = {
+  root: () => {
+    return firebase.storage()
+  },
+  images: (fileName) => {
+    return firebase
+      .storage().ref()
+      .child(
+        `/images/${fileName}`
+      )
+  }
+}
+
+export { db, storage, GeoPoint, TimeStamp, CurrentTime, auth, FieldValue }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import { firestorePlugin } from 'vuefire'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
+
+Vue.use(firestorePlugin)
 
 new Vue({
   render: h => h(App),
