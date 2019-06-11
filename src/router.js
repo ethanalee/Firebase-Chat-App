74c7a1ee4f3b82abb5910abc8fd66c6a56f990dd
Frank Flitton <frank@tdlab.io>
2019-06-11 12:01:25 -0400

    Rough Layout

--- src/router.js
+++ src/router.js
@@ -22,6 +22,14 @@ const router =  new Router({
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     },
     {
+      path: '/home',
+      name: 'home',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Home.vue')
+    },
+    {
       path: '/login',
       name: 'login',
       // route level code-splitting
