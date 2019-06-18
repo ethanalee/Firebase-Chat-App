4fb2a226f3babc8e7027ef2cfdb30cdf04ded8ca
Frank Flitton <frank@tdlab.io>
2019-06-18 16:27:03 -0400

    Headline

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,16 +5,17 @@
 
         <v-layout row wrap fill-height>
           <v-flex xs12>
-            <v-card>
-              <v-card-title primary-title>
-                <div>
-                  <h3 class="headline mb-0">Friend Search</h3>
-                  <div>
-                    <v-form
-                      ref="form"
-                      v-model="valid"
-                      lazy-validation
-                    >
+            <v-card class="pa-0 ma-0">
+              <v-card-title primary-title class="pa-0 ma-0">
+
+                  <v-form
+                    ref="form"
+                    v-model="valid"
+                    lazy-validation
+                    class="add-friend-form pl-3"
+                  >
+                    <v-layout row fill-height>
+                      <v-flex xs8>
                     <v-autocomplete
                         v-model="friendsAdded"
                         return-object
@@ -26,22 +27,27 @@
                         prepend-icon="mdi-database-search"
                         label="Add Friends"
                         :filter="customFilter"
-                        outline
                         no-data-text="No Friends"
                       ></v-autocomplete>
-                    </v-form>
-                  </div>
-                </div>
+                      </v-flex>
+                      <v-flex
+                        text-xs-right
+                        xs4
+                      >
+                      <v-btn
+                        :disabled="!valid"
+                        color="success"
+                        @click="validate"
+                        class="fill-button ma-0"
+                        fab
+                      >
+                        +
+                    </v-btn>
+                    </v-flex>
+                    </v-layout>
+                  </v-form>
+
               </v-card-title>
-              <v-card-actions>
-                <v-btn
-                    :disabled="!valid"
-                    color="success"
-                    @click="validate"
-                  >
-                    Add Friend
-                </v-btn>
-              </v-card-actions>
             </v-card>
 
           </v-flex>
@@ -94,6 +100,24 @@
 <style>
 .home-page {
   height: calc(100vh - 56px)
+}
+.add-friend-form {
+  width: 100%;
+}
+.add-friend-form .v-input__prepend-outer,
+.add-friend-form .v-text-field__details,
+.add-friend-form .v-input__append-inner
+{
+  display: none;
+}
+.add-friend-form .v-input__slot {
+  margin-bottom: 0;
+}
+.fill-button {
+  width: 100%;
+  height: 100%;
+  border-radius: 0;
+  float: right;
 }
 </style>
 
