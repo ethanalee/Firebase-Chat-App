0ce63aa5fcacad153ed6f0676d9b3ba57a0828b3
Frank Flitton <frank@tdlab.io>
2019-06-18 16:50:44 -0400

    Spacing

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,47 +1,47 @@
 <template>
-    <v-container fluid>
+  <v-container fluid>
     <v-layout align-center justify-center>
-        <v-flex xs12 sm8 md4>
+      <v-flex xs12 sm8 md4>
         <v-card class="elevation-12">
-            <v-toolbar dark color="primary">
+          <v-toolbar dark color="primary">
             <v-toolbar-title>Sign Up form</v-toolbar-title>
             <v-spacer></v-spacer>
-            </v-toolbar>
-            <v-card-text>
+          </v-toolbar>
+          <v-card-text>
             <v-form>
-                <v-text-field
+              <v-text-field
                 prepend-icon="edit"
                 name="displayName"
                 label="Display Name"
                 type="text"
                 v-model="displayName"
-                ></v-text-field>
-                <v-text-field
+              ></v-text-field>
+              <v-text-field
                 prepend-icon="person"
                 name="email"
                 label="Email"
                 type="text"
                 v-model="email"
-                ></v-text-field>
-                <v-text-field
+              ></v-text-field>
+              <v-text-field
                 id="password"
                 prepend-icon="lock"
                 name="password"
                 label="Password"
                 type="password"
                 v-model="password"
-                ></v-text-field>
+              ></v-text-field>
             </v-form>
-            </v-card-text>
+          </v-card-text>
 
-            <v-card-actions>
+          <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn color="primary" @click="emailSignup">Sign Up</v-btn>
-            </v-card-actions>
+          </v-card-actions>
         </v-card>
-        </v-flex>
+      </v-flex>
     </v-layout>
-    </v-container>
+  </v-container>
 </template>
 
 <script>
